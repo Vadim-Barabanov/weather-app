8b6d479f7af60f47c99251beab513330c46a7839
Vadim Barabanov <introvert.png@gmail.com>
2021-03-03 22:44:30 +0200

    nothing about security

--- src/api/api.ts
+++ src/api/api.ts
@@ -1,18 +1,18 @@
-import axios from 'axios';
-import { INITIAL_UNITS } from '../common/constants';
-import { TUnits } from '../common/types';
+import axios from 'axios'
+import { INITIAL_UNITS } from '../common/constants'
+import { TUnits } from '../common/types'
 
-const API_KEY = '4a626d4e8e8d778563b07254fa68dbe0';
+const API_KEY = process.env.REACT_APP_API_KEY
 
 const instance = axios.create({
     baseURL: 'https://api.openweathermap.org/data/2.5/forecast/',
-});
+})
 
 export const weatherAPI = {
     async getWeatherData(city: string, units: TUnits = INITIAL_UNITS) {
         const res = await instance.get(
             `?q=${city}&appid=${API_KEY}&units=${units}`
-        );
-        return res;
+        )
+        return res
     },
-};
+}
